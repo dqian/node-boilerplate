57f8d5552a5e841e32934a79c0d9ecf8e78073fe
Dennis Qian <orangemoney52@gmail.com>
2020-07-09 23:39:35 -0700

    adding db call to health check

--- src/packages/api/resources/default/controller.ts
+++ src/packages/api/resources/default/controller.ts
@@ -1,9 +1,15 @@
 import { NextFunction, Request, Response } from 'express'
 import * as httpStatus from 'http-status'
+import { getConnection } from 'typeorm';
+import { User } from '~/packages/database/models/user';
 
 export const health = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  const UsersTable = getConnection().getRepository(User);
+  const totalUsers = await UsersTable.createQueryBuilder().getCount();
+  
   return res.status(httpStatus.OK).json({ 
     status: 'OK',
-    version: 1.2
+    version: 0.1,
+    c: totalUsers
   });
 }
